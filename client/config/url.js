7cdc7141a2354943ad96514bd500c5253075f267
diandian <salle@126.com>
2020-06-29 10:44:58 +0800

    fix: axios请求前缀即使在生产环境也是localhost的问题

--- client/config/url.js
+++ client/config/url.js
@@ -1,5 +1,6 @@
 const DEV_URL = 'http://localhost:3000'
 const MOCK_URL = '' // mock数据获取地址
 const PRO_URL = '/'
+const URL = process.env.NODE_ENV === 'development' ? DEV_URL : PRO_URL
 
-export default DEV_URL
+export default URL
